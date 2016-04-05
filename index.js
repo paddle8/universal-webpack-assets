9b3a76c3baee65f0a27fc23a9b087942923663d3
George Brassey <george.brassey@paddle8.com>
2016-04-05 15:53:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/* eslint-disable object-shorthand */
+
+'use strict'; // eslint-disable-line strict
+
+const fs = require('fs');
+const path = require('path');
+
+function universalWebpackAssetsPluginApply(options, compiler) {
+  compiler.plugin('done', function WriteStatsPluginDone(stats) {
+    const defaults = {
+      manifestFile: 'universal-assets.json',
+      regex: /\.(jpe?g|png|gif|svg)$/,
+    };
+
+    const config = Object.assign({}, defaults, options, this.options.universalWebpackAssetsConfig);
+
+    const publicPath = this.options.output.publicPath;
+    const json = stats.toJson();
+
+    const getCompiledAsset = function getCompiledAsset(asset) {
+      let compiled;
+      if (asset.assets.length) {
+        compiled = `module.exports = "${publicPath}${asset.assets[0]}"`;
+      } else {
+        compiled = asset.source;
+      }
+      return compiled;
+    };
+
+    // Find compiled assets in modules
+    // it will be used to map original filename to the compiled one
+    // for server side rendering
+    const assetRegex = config.regex;
+    const assets = json.modules
+      .filter(module => assetRegex.test(module.name))
+      .map(asset =>
+        ({
+          resourcePath: path.join(stats.compilation.options.context, asset.name),
+          compiled: getCompiledAsset(asset),
+        })
+      );
+
+    const content = { assets };
+
+    const manifestFile = config.manifestFile;
+
+    fs.writeFileSync(manifestFile, JSON.stringify(content));
+  });
+}
+
+module.exports = function universalWebpackAssetsPlugin(options) {
+  return {
+    apply: universalWebpackAssetsPluginApply.bind(this, options),
+  };
+};
---
+++ loader.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function universalWebpackAssetsLoader() {
+  const defaults = {
+    manifestFile: path.join(this.options.context, 'universal-assets.json'),
+  };
+
+  const config = Object.assign({}, defaults, this.options.universalWebpackAssetsConfig);
+
+  this.cacheable();
+  const manifestPath = config.manifestFile;
+  this.addDependency(manifestPath);
+  const manifest = JSON.parse(fs.readFileSync(manifestPath).toString());
+  const assets = manifest.assets;
+  const asset = assets.filter(i => i.resourcePath === this.resourcePath)[0];
+  return asset.compiled;
+};
